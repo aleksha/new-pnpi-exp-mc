ff621812a7c92a97d43cb66cd3231a9b2f1b62eb
Aleksei Dziuba <adzyuba@cern.ch>
2023-03-30 10:34:27 +0300

    [code] every 2nd particle is the beam one now

--- src/CSCPrimaryGeneratorAction.cc
+++ src/CSCPrimaryGeneratorAction.cc
@@ -26,7 +26,7 @@ CSCPrimaryGeneratorAction::CSCPrimaryGeneratorAction()
   fParticleGun->SetParticleDefinition(particle);
   fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
   fParticleGun->SetParticlePosition( G4ThreeVector(0.,0.,-1999.5*mm) );
-  fParticleGun->SetParticleEnergy(1000.*MeV);
+  fParticleGun->SetParticleEnergy(BEAM_ENERGY*MeV);
   ev_id=0;
   myGEN .open( "gen.data" , std::ios::trunc);
 }
@@ -41,32 +41,36 @@ void CSCPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
   //this function is called at the begining of ecah event
 
 //  fAlpha = 0.; sinAlpha = 0.; cosAlpha = 1.; psi = 0.;
-
 //  fAlpha = 0.4*0.5236+CLHEP::RandGauss::shoot(0,0.003);
+
   fAlpha = 0.3*G4UniformRand();
   fAlpha = std::sqrt(fAlpha*fAlpha);
   sinAlpha = std::sin(fAlpha);
   cosAlpha = std::sqrt(1. - sinAlpha*sinAlpha);
   psi = G4UniformRand()*2.*3.14159265;
-//  psi = 0.;
 
-  if(SMEAR_BEAM){
-    do{
-      fVx = G4UniformRand();
-      fVy = G4UniformRand();
-    }
-    while (fVx*fVx + fVy*fVy > 1);
-  } else{ fVx=0.; fVy=0.; };
-
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(sinAlpha*std::cos(psi),sinAlpha*std::sin(psi),cosAlpha));
+  if( !(ev_id%2) ){
+    if(SMEAR_BEAM){
+      do{
+        fVx = G4UniformRand();
+        fVy = G4UniformRand();
+      }
+      while (fVx*fVx + fVy*fVy > 1);
+    } else{ fVx=0.; fVy=0.; };
+    fParticleGun->SetParticleMomentumDirection(G4ThreeVector(sinAlpha*std::cos(psi),sinAlpha*std::sin(psi),BEAM_Z_DIRECT*cosAlpha));
+  }
+  else{
+    fParticleGun->SetParticleMomentumDirection( G4ThreeVector(0.0,0.0,-BEAM_Z_DIRECT*1.0) );
+  }
   fParticleGun->SetParticlePosition( G4ThreeVector(fVx*10.*mm,fVy*10.*mm,VERTEX_POS*mm) );
-//  fParticleGun->SetParticlePosition( G4ThreeVector(fVx*10.*mm,fVy*10.*mm,-1999.5*mm) );
+
 
   if(myGEN.is_open())
        myGEN << ev_id  << " " <<  fVx*10.  << " " << fVy*10.  << " " << VERTEX_POS << " "
-             << sinAlpha*std::cos(psi) << " " << sinAlpha*std::sin(psi) << " " << cosAlpha << " 1000. " << G4endl;
-//       myGEN << ev_id  << " " <<  fVx*10.  << " " << fVy*10.  << " -1999.5 "
-//             << sinAlpha*std::cos(psi) << " " << sinAlpha*std::sin(psi) << " " << cosAlpha << " 1000. " << G4endl;
+             << pow(0,ev_id%2)*sinAlpha*std::cos(psi)  << " "
+             << pow(0,ev_id%2)*sinAlpha*std::sin(psi)  << " "
+             << pow(-1,ev_id%2)*BEAM_Z_DIRECT*pow( cosAlpha,(ev_id+1)%2 ) << " " 
+             << BEAM_ENERGY << G4endl;
 
   ev_id++;
   fParticleGun->GeneratePrimaryVertex(anEvent);
